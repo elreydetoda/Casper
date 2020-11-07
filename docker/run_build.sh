d18bc2bed7260a29e9ee7f90fd05f012b977e305
elreydetoda <unc741@gmail.com>
2020-11-07 04:57:40 -0500

    adding dev differentiator

--- docker/run_build.sh
+++ docker/run_build.sh
@@ -11,17 +11,24 @@ function run_build(){
 function move_zip(){
 
     dist_folder="${PWD}/dist"
-    new_theme="${dist_folder}/$(date -Iseconds)_casper-personal.zip"
-    theme_link="${dist_folder}/casper-personal.zip"
+    new_theme="${dist_folder}/${current_branch:-}$(date -Iseconds)_casper-personal.zip"
+    theme_link="${dist_folder}/${current_branch}casper-personal.zip"
     mv "${dist_folder}/casper.zip" "${new_theme}"
     ln -sf "${new_theme}" "${theme_link}"
     ls "${theme_link}"
 
 }
 
+function branch_check(){
+  if [[ "$(git branch --show-current)" == feat/* ]]; then
+    current_branch='dev-'
+  fi
+}
+
 function main(){
 
   run_build "${@}"
+  branch_check
   move_zip
 
 }
